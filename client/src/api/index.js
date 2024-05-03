516225667e222ecdcef103a75d808da845b6daf3
Harsh Shukla <shuklaharsh83267@gmail.com>
2024-05-03 19:44:22 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+axios.defaults.withCredentials=true;
+const API = axios.create({ baseURL: 'https://social-media-api-lyart.vercel.app' });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
