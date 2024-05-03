c06e688f58975112bb527d99893e4d655cc88de8
Harsh Shukla <shuklaharsh83267@gmail.com>
2024-05-03 19:31:11 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,13 @@ const app = express();
 
 app.use(express.json({ limit: '30mb', extended: true }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
-app.use(cors());
+app.use(cors(
+  {
+    origin:[""],
+    methods:["POST","GET"],
+    credentials:true
+  }
+));
 
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
