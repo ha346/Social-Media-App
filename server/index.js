f42501f5830687dff5acacf5e75ed58c021945f2
Harsh Shukla <shuklaharsh83267@gmail.com>
2024-05-03 19:50:49 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,13 +11,7 @@ const app = express();
 
 app.use(express.json({ limit: '30mb', extended: true }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
-app.use(cors(
-  {
-    origin:["https://social-media-api-lyart.vercel.app"],
-    methods:["POST","GET"],
-    credentials:true
-  }
-));
+app.use(cors());
 
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
